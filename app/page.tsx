17d7ce85932db2c9014b280d8d9cada1f80bff4c
surf-kirill-berenfus <berenfus@surf.dev>
2023-10-29 16:09:21 +0300

    Remove ButtonsGroup component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,7 @@
-import { ButtonsGroup } from 'shared/ui/ButtonsGroup';
-
 export default function Home() {
     return (
         <div>
             <div>Haiiiii ^_^ hi!! hiiiiii &lt;3 haiiiiiii hii :3</div>
-            <ButtonsGroup />
         </div>
     );
 }
